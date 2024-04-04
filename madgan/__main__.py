74fe9d67f4d20f68e3d99b92f3fe676abb06799c
Inno Jia <jdgets111@gmail.com>
2024-04-04 11:49:02 -0600

    Add data preprocessing

---
+++ madgan/__main__.py
@@ -0,0 +1,16 @@
+import typer
+from madgan.train import train
+from madgan.preprocess import swat
+
+_madgan_cli = typer.Typer(name="MAD-GAN CLI")
+_madgan_cli.command(name="train")(train)
+_madgan_cli.command(name="swat")(swat)
+
+
+@_madgan_cli.callback()
+def main() -> None:
+    """MAD-GAN Command Line Interface."""
+
+
+if __name__ == "__main__":
+    _madgan_cli()
---
+++ madgan/preprocess.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+
+
+def swat(raw_data_path: str,
+         sheet_name: str = 'Combined Data',
+         n_features: int = 10,
+         output_csv: str = './data/swat_data.csv',
+         skip_cnt: int = 21600) -> None:
+    """Preprocess the raw data from the SWaT dataset and generate a CSV file.
+
+    Args:
+        raw_data (str): Path to the raw data file.
+        n_features (int): Number of features to consider.
+        output_csv (str): Path to the output CSV file.
+        skip_cnt (int, optional): Number of rows to skip at the beginning 
+            of the file to maintain system stability. Default to 21600.
+    """
+    df = pd.read_excel(raw_data_path, sheet_name=sheet_name, header=1)
+    assert df.isnull().sum().sum() == 0, 'Missing values in the data'
+    df.columns = df.columns.str.strip()
+    f_columns = df.columns.tolist()
+    f_columns.remove('Normal/Attack')
+    f_columns.remove('Timestamp')
+    print(f"All features columns ({len(f_columns)}): {f_columns}")
+
+    df['Normal/Attack'] = df['Normal/Attack'].replace('\s+', '', regex=True)
+    df.loc[:, 'label'] = df['Normal/Attack'].map({'Normal': 0, 'Attack': 1})
+    df = df.drop(['Normal/Attack', 'Timestamp'], axis=1)
+
+    m, n = df.shape
+    assert df.shape[0] > skip_cnt, f"Number of records is less than {skip_cnt}"
+    print(f'Read excel data: {df.shape},\nColumns: {df.columns}')
+    print(f"Class counts: {df['label'].value_counts()}")
+
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    df[f_columns] = scaler.fit_transform(df[f_columns])
+    # Skip the first 21600 records to maintain system stability
+    samples = df.iloc[skip_cnt:, :-1].copy()
+    labels = df.iloc[skip_cnt:, -1].copy()
+    print(f"Data shape after skipping {skip_cnt} records: {samples.shape}")
+
+    pca = PCA(n_components=n_features, svd_solver='full')
+    pca.fit(samples)
+    ex_var = pca.explained_variance_ratio_
+    pc = pca.components_
+
+    # Project the data to the new feature space
+    projected_sample = samples @ pc.T
+    print(f"Projected data shape: {projected_sample.shape}")
+    print(f"Explained variance: {ex_var},\nHead: {projected_sample[:5]}")
+    projected_sample.to_csv(output_csv, index=False)
