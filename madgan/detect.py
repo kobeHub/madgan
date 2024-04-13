e5ffe2ec5b500235ca5b054527954807f232d92e
Inno Jia <jdgets111@gmail.com>
2024-04-13 12:08:06 -0600

    version 7: test

--- madgan/detect.py
+++ madgan/detect.py
@@ -90,13 +90,12 @@ def calculate_metrics(pred_labels: torch.Tensor, true_labels: torch.Tensor) -> T
     # Reshape labels
     pred_labels = pred_labels.view(-1)
     true_labels = true_labels.view(-1)
-    if torch.any(true_labels == 1):
-        print(f'The true labels count: {true_labels.count_nonzero()},'
-              f' The predicted labels count: {pred_labels.count_nonzero()}, '
-              f'True positive count: {(pred_labels == true_labels).sum()}, '
-              f'False positive count: {(pred_labels != true_labels).sum()}, '
-              f'False negative count: {(pred_labels != true_labels).sum()}, '
-              f'True negative count: {(pred_labels == true_labels).sum()}')
+    print(f'The true labels count: {true_labels.count_nonzero()},'
+          f' The predicted labels count: {pred_labels.count_nonzero()}, '
+          f'True positive count: {(pred_labels == true_labels).sum()}, '
+          f'False positive count: {(pred_labels != true_labels).sum()}, '
+          f'False negative count: {(pred_labels != true_labels).sum()}, '
+          f'True negative count: {(pred_labels == true_labels).sum()}')
 
     # Compute metrics
     precision = F.precision(pred_labels, true_labels,
